5c0cb8427053d740960bfb0743e8f75d6fe3b3b7
Guled <guletkadar@gmail.com>
2023-08-21 15:32:56 +0300

    feat: model

---
+++ backend/model/todoModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const todoSchema = new mongoose.Schema({
+    todo : {
+        type: String,
+        required: true
+    }
+})
+
+
+
+module.exports = mongoose.model("Todo", todoSchema)
