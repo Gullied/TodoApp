5a36c06db667a11dbdd007f2e9591dc099db94c7
Guled <guletkadar@gmail.com>
2023-08-23 22:11:49 +0300

    feat Add input feild add button

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Todo App</h1>
+        <div className="Top">
+          <input type="text" placeholder="Type your Todo..."/>
+          <div className="Add">Add</div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
